c397c288c692124de0dbf8ccdf1ff8a0007a01e6
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-15 21:30:23 +0000

    working on hompage. got video background and centered title.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,26 @@
 export default function Homepage() {
     return (
         <>
-            <p className="text-2xl text-green-700">Homepage</p>
+            <div className="relative w-full h-screen overflow-hidden">
+                {/* Backgroud video */}
+                <video className="absolute top-0 left-0 w-full h-full object-cover" autoPlay loop muted>
+                    <source src="../../images/test-video-2.mp4" type="video/mp4" />
+                    Your browser does not support this video tag
+                </video>
+
+                {/* Overlay  */}
+                <div className="absolute inset-0 bg-black/50">
+                </div>
+
+                {/* Content  */}
+                <div className="absolute w-full h-full flex items-center justify-center">
+                    <h1 className="text-[150px] text-primaryTextColor text-center uppercase font-bold font-inter tracking-wide">ARCTIC</h1>
+                    {/* <p className="text-4xl px-2 text-white">Software Developer</p> */}
+                </div>
+            </div>
+
+            
+
         </>
     )
 }
